3ec70d574dc0801b201eca675089462992c3f3cd
Breyanna Vass <breyannavass@gmail.com>
2019-12-03 20:30:49 -0500

    making the brightness work

--- app.js
+++ app.js
@@ -9,3 +9,18 @@ $("#blur input").onchange = function() {
   var heading = $("#blur h3");
   heading.textContent = "blur(" + this.value + "px)";
 };
+
+$("#blur input").value = 2;
+$("#blur input").onchange();
+
+// brightness
+$("#brightness input").onchange = function() {
+  var img = $("#brightness img");
+  img.style.webkitFilter = "brightness(" + this.value + "%" + ")";
+
+  var heading = $("#brightness h3");
+  heading.textContent = "brightness(" + this.value + "%)";
+};
+
+$("#brightness input").value = 30;
+$("#brightness input").onchange();
